729daabeaade55e27a03975b8b7d5499f5b760ed
Josh Schreuder <jschreuder@mediaocean.com>
2017-07-14 11:12:11 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Serilog.Enrichers.Memory
+
+The memory usage enricher for Serilog.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ihq58voxyfwfanyg?svg=true)](https://ci.appveyor.com/project/serilog/serilog-enrichers-process) [![NuGet Version](http://img.shields.io/nuget/v/Serilog.Enrichers.Process.svg?style=flat)](https://www.nuget.org/packages/Serilog.Enricher.Process/)
+
+
+To use the enricher, first install the NuGet package:
+
+```powershell
+Install-Package Serilog.Enrichers.Memory
+```
+
+# Documentation
+
+Copyright &copy; 2017 Josh Schreuder - Provided under the [Apache License, Version 2.0](http://apache.org/licenses/LICENSE-2.0.html).
---
+++ src/Serilog.Enrichers.Process/ProcessLoggerConfigurationExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2013-2016 Serilog Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using System;
+using Serilog.Configuration;
+using Serilog.Enrichers;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Extends <see cref="LoggerConfiguration"/> to add enrichers related to process.
+    /// capabilities.
+    /// </summary>
+    public static class ProcessLoggerConfigurationExtensions
+    {
+        /// <summary>
+        /// Enrich log events with memory usage/>.
+        /// </summary>
+        /// <param name="enrichmentConfiguration">Logger enrichment configuration.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+        public static LoggerConfiguration WithMemoryUsage(
+           this LoggerEnrichmentConfiguration enrichmentConfiguration)
+        {
+            if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+            return enrichmentConfiguration.With<MemoryUsageEnricher>();
+        }
+    }
+}
